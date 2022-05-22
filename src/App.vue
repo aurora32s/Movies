69bbe6da986bfec2e79c2bd12975dbaac9b6c8b6
aurora32s <aurora32s@naver.com>
2022-05-22 17:59:39 +0900

    vue 파일에서 css/scss 코드 분리

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,6 @@
         <router-view></router-view>
     </div>
 </template>
-
 <style lang="scss">
-@import "~/assets/scss/_color.scss";
-
-html, body {
-    padding: 0;
-    margin: 0;
-}
-html, body, #main, #app {
-    height: 100%;
-}
-body {
-    background-color: $background;
-}
+@import './assets/scss/style.scss';
 </style>
